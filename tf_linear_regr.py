6509ec7f54dd7bfcc6d25ec4cc0e0a580aadcc42
Peter Jochem <peterpennrichjochem@gmail.com>
2020-05-04 09:42:13 -0500

    Updated regression demo

--- tf_linear_regr.py
+++ tf_linear_regr.py
@@ -7,18 +7,20 @@ x_batch = np.linspace(0, 100, 100)
 y_batch = 1.5 * x_batch + np.random.randn(*x_batch.shape) * 5.0 + 0.5
 
 # Define the computational graph
-x = tf.placeholder(tf.float32, shape=(None, ), name = 'x')
-y = tf.placeholder(tf.float32, shape=(None, ), name = 'y')
+x = tf.placeholder(tf.float32, shape=(None, ), name = 'x') # The input to the function
+y = tf.placeholder(tf.float32, shape=(None, ), name = 'y') # The associated label
 
+# Learable parameters of the model
 w = tf.Variable(np.random.normal(), name = 'W')
 b = tf.Variable(np.random.normal(), name = 'b')
-		
+
+# Node that is the model's prediction
 y_pred = tf.add(tf.multiply(w, x), b)
 
-# Now let's define the loss function which we are going to train the model on
+# Graph's node which is the loss
 loss = tf.reduce_mean(tf.square(y_pred - y))
 
-# Add a gradient to the computational graph
+# Add gradient nodes to the computational graph
 d_loss_dx = tf.gradients(loss, x)[0]
 d_loss_dw = tf.gradients(loss, w)[0]
 
@@ -30,9 +32,11 @@ train_op = optimizer.minimize(loss)
 # Run data and minimze loss
 epochs = 10
 with tf.Session() as sess:
+
     # Initialize the variables
     sess.run(tf.global_variables_initializer())
-         
+    
+    # Tells model what the input/output pairs are
     feed_dict = {x: x_batch, y: y_batch }
 		
     for i in range(epochs):
@@ -45,16 +49,17 @@ with tf.Session() as sess:
         # as the data in the feed_dictionary
         print(i, "loss:", loss.eval(feed_dict) )
 
-    x_test = np.linspace(0, 100, 100)
-    y_test = sess.run(y_pred, {x : x_batch})
-    
-
+ 
     # This is a example of how to compute derivatives using the learned model
-    grad =  sess.run(d_loss_dw, {x : x_batch, y: y_batch } )
+    grad = sess.run(d_loss_dw, {x : x_batch, y: y_batch } )
 
     print("")
     print("The gradient of loss wrt w is ")
     print(grad)
+    
+    # Generate trend line using the learned function
+    x_test = np.linspace(0, 100, 100)
+    y_test = sess.run(y_pred, {x : x_test})
 
     # Plot the data
     plt.plot( x_test, y_test, color = "red" )
