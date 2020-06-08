bada7fe7400ae3e3b443b7d6c38cc9a5dc4be78c
Peter Jochem <peterpennrichjochem@gmail.com>
2020-06-07 21:45:06 -0500

    Added images/graphs

--- README.md
+++ README.md
@@ -1,14 +1,5 @@
-# Summary 
-I wanted to generate a similiar dataset to the one Juntao generated to make graphs 2A-D in Chen Li's paper. This required me to read and understand the Chrono simulation. I ran into a few errors which I could not resolve. The longer description elaborates. I talked to Dan and Juntao and it likely makes more sense if instead of re-generating data similiar to Juntao's, I use his data. The Chrono code is tough to modify and if Juntao understands it quite well, I am proabbly more useful spending time buildig out the machine learning API. I have part of Juntao's data which varies (attack angle, intrusion angle) and was used to replicate Chen Li's graphs. From this, I trained the network to map (x, y, z, attackAngle, intrusionAngle) to (F_x, F_y, F_z). From this, I can compute the desired N/cm^3 
+# Summary
+Juntao gave me the data he used to replicate Chen Li's graphs
 
-# Plan For Next Week
-Use Juntao's data to learn the same function and compare it to Chen Li's and Juntao's graphs
-Dan asked for a description of the database along with a pdf of how to use it
-
-# More Detailed Summary
-This week I tried to create a dataset with many trials, each with a diffrent (attack angle, intrusion angle) pair. 
-Things I did this week on route to creating these graphs. I modified the Chrono simulation to allow me to set the angle of attack and the intrusion angle. I then added command line arguments so that I can run an experiment with variable attack angle and instrusion angle. I then wrote a bash script that runs the Chrono experiment many times with variable attack angles and intrusion angles.
-
-# Setbacks
-I opened up the file where I was storing the 3 vectors at each time step of the GRF. They are all 0. I then checked the demo which Juntao gave me to build on top of and that demo also has all (0, 0, 0) GRF. I looked into the API but could not sort it out. There must be some or a few small things I am doing wrong. I will work on resolving this. I spent a lot of time trying to understand the API and why I was getting (0, 0, 0) GRF. I retraced my steps by copying the most recent simulation code and modifying it to suit my needs.
+# Results
 
--- replicateChenLiGraphs/processData.py
+++ replicateChenLiGraphs/processData.py
@@ -187,17 +187,37 @@ x = tf.placeholder(tf.float32, shape=(None, 2), name = 'x')
 # Outputs are [F x / depth, Force z / depth]
 y = tf.placeholder(tf.float32, shape=(None, 2), name = 'y')
 
-W1 = tf.Variable(tf.random_normal([2, 30], stddev = 0.03), name = 'W1')
-b1 = tf.Variable(tf.random_normal([30]), name ='b1')
+W1 = tf.Variable(tf.random_normal([2, 100], stddev = 0.03), name = 'W1')
+b1 = tf.Variable(tf.random_normal([100]), name = 'b1')
+
+W2 = tf.Variable(tf.random_normal([100, 70], stddev = 0.03), name = 'W2')
+b2 = tf.Variable(tf.random_normal([70]), name = 'b2')
+
+W3 = tf.Variable(tf.random_normal([70, 40], stddev = 0.03), name = 'W3')
+b3 = tf.Variable(tf.random_normal([40]), name = 'b3')
 
 # and the weights connecting the hidden layer to the output layer
-W2 = tf.Variable(tf.random_normal([30, 2], stddev = 0.03), name = 'W2')
-b2 = tf.Variable(tf.random_normal([2]), name = 'b2')
+W4 = tf.Variable(tf.random_normal([40, 15], stddev = 0.03), name = 'W4')
+b4 = tf.Variable(tf.random_normal([15]), name = 'b4')
 
-hidden_out = tf.add(tf.matmul(x, W1), b1)
-hidden_out = tf.nn.relu(hidden_out)
+# and the weights connecting the hidden layer to the output layer
+W5 = tf.Variable(tf.random_normal([15, 2], stddev = 0.03), name = 'W5')
+b5 = tf.Variable(tf.random_normal([2]), name = 'b5')
 
-y_pred = tf.add(tf.matmul(hidden_out, W2), b2)
+
+hidden_out1 = tf.add(tf.matmul(x, W1), b1)
+hidden_out1 = tf.nn.relu(hidden_out1)
+
+hidden_out2 = tf.add(tf.matmul(hidden_out1, W2), b2)
+hidden_out2 = tf.nn.relu(hidden_out2)
+
+hidden_out3 = tf.add(tf.matmul(hidden_out2, W3), b3)
+hidden_out3 = tf.nn.relu(hidden_out3)
+
+hidden_out4 = tf.add(tf.matmul(hidden_out3, W4), b4)
+hidden_out4 = tf.nn.relu(hidden_out4)
+
+y_pred = tf.add(tf.matmul(hidden_out4, W5), b5)
 
 # loss = tf.nn.l2_loss(y_pred - y) # / (len(myDataSet.allInstances))
 loss = tf.reduce_mean(tf.square(y_pred - y))
@@ -208,7 +228,7 @@ print("")
 print("")
 
 # 0.00000001
-optimizer = tf.train.GradientDescentOptimizer(0.0005)
+optimizer = tf.train.GradientDescentOptimizer(0.001)
 train_op = optimizer.minimize(loss)
 
 
@@ -229,7 +249,7 @@ saver = tf.train.Saver()
 
 
 # Train on the training set
-epochs = 1000
+epochs = 20000
 with tf.Session() as sess:
 
     # Initialize the variables
